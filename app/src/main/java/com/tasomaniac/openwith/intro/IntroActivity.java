e6c673f2b1bbb78a041669e6f26b70c341c15b02
Said Tahsin Dane <tasomaniac@gmail.com>
2015-12-24 14:36:04 +0200

    IntroActivity is added.

---
+++ app/src/main/java/com/tasomaniac/openwith/intro/IntroActivity.java
@@ -0,0 +1,35 @@
+package com.tasomaniac.openwith.intro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    public void init(@Nullable Bundle savedInstanceState) {
+
+//        addSlide();
+    }
+
+    @Override
+    public void onSkipPressed() {
+
+    }
+
+    @Override
+    public void onNextPressed() {
+
+    }
+
+    @Override
+    public void onDonePressed() {
+
+    }
+
+    @Override
+    public void onSlideChanged() {
+
+    }
+}
