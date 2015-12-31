ac25f5659f5c36ea8e207b3d23de9051c0ab8d64
Said Tahsin Dane <tasomaniac@gmail.com>
2015-12-31 17:23:28 +0200

    AppIntro fake page is added.

--- app/src/main/java/com/tasomaniac/openwith/intro/IntroActivity.java
+++ app/src/main/java/com/tasomaniac/openwith/intro/IntroActivity.java
@@ -2,20 +2,24 @@ package com.tasomaniac.openwith.intro;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 
 import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.tasomaniac.openwith.R;
 
 public class IntroActivity extends AppIntro {
 
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
 
-//        addSlide();
+        addSlide(AppIntroFragment.newInstance("Welcome", "To Be Implemented", R.drawable.ic_action_done,
+                ContextCompat.getColor(this, R.color.theme_primary)));
     }
 
     @Override
     public void onSkipPressed() {
-
+        finish();
     }
 
     @Override
@@ -25,7 +29,7 @@ public class IntroActivity extends AppIntro {
 
     @Override
     public void onDonePressed() {
-
+        finish();
     }
 
     @Override
