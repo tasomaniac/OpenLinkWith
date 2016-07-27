0e01695a47163418c455b83925b50efc43b1028f
Said Tahsin Dane <tasomaniac@gmail.com>
2016-07-28 01:21:15 +0200

    Lint error fixes

--- app/src/main/java/com/tasomaniac/openwith/intro/AppIntro.java
+++ app/src/main/java/com/tasomaniac/openwith/intro/AppIntro.java
@@ -1,26 +1,16 @@
 package com.tasomaniac.openwith.intro;
 
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.support.annotation.ColorInt;
-import android.support.annotation.ColorRes;
-import android.support.annotation.DrawableRes;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tasomaniac.openwith.R;
@@ -29,26 +19,22 @@ import java.util.List;
 import java.util.Vector;
 
 public abstract class AppIntro extends AppCompatActivity {
-    public final static int DEFAULT_COLOR = 1;
 
-    protected PagerAdapter mPagerAdapter;
-    protected AppIntroViewPager pager;
-    protected List<Fragment> fragments = new Vector<>();
-    protected List<ImageView> dots;
-    protected int slidesNumber;
-    protected Vibrator mVibrator;
-    protected CircularIndicatorView mController;
-    protected boolean isVibrateOn = false;
-    protected int vibrateIntensity = 20;
-    protected boolean skipButtonEnabled = true;
-    protected boolean baseProgressButtonEnabled = true;
-    protected boolean progressButtonEnabled = true;
-    protected int selectedIndicatorColor = DEFAULT_COLOR;
-    protected int unselectedIndicatorColor = DEFAULT_COLOR;
-    protected View skipButton;
-    protected View nextButton;
-    protected View doneButton;
-    protected int savedCurrentItem;
+    private PagerAdapter mPagerAdapter;
+    private AppIntroViewPager pager;
+    private List<Fragment> fragments = new Vector<>();
+    private int slidesNumber;
+    private Vibrator mVibrator;
+    private CircularIndicatorView mController;
+    private boolean isVibrateOn = false;
+    private int vibrateIntensity = 20;
+    private boolean skipButtonEnabled = true;
+    private boolean baseProgressButtonEnabled = true;
+    private boolean progressButtonEnabled = true;
+    private View skipButton;
+    private View nextButton;
+    private View doneButton;
+    private int savedCurrentItem;
 
     @Override
     final protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +58,7 @@ public abstract class AppIntro extends AppCompatActivity {
 
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(@NonNull View v) {
+            public void onClick(View v) {
                 if (isVibrateOn) {
                     mVibrator.vibrate(vibrateIntensity);
                 }
@@ -82,7 +68,7 @@ public abstract class AppIntro extends AppCompatActivity {
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(@NonNull View v) {
+            public void onClick(View v) {
                 if (isVibrateOn) {
                     mVibrator.vibrate(vibrateIntensity);
                 }
@@ -93,7 +79,7 @@ public abstract class AppIntro extends AppCompatActivity {
 
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(@NonNull View v) {
+            public void onClick(View v) {
                 if (isVibrateOn) {
                     mVibrator.vibrate(vibrateIntensity);
                 }
@@ -165,7 +151,7 @@ public abstract class AppIntro extends AppCompatActivity {
     }
 
 
-    protected void restoreLockingState(Bundle savedInstanceState) {
+    private void restoreLockingState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         this.baseProgressButtonEnabled = savedInstanceState.getBoolean("baseProgressButtonEnabled");
         this.progressButtonEnabled = savedInstanceState.getBoolean("progressButtonEnabled");
@@ -176,36 +162,14 @@ public abstract class AppIntro extends AppCompatActivity {
         pager.setLockPage(savedInstanceState.getInt("lockPage"));
     }
 
-    public AppIntroViewPager getPager() {
-        return pager;
-    }
-
     private void initController() {
         mController = (CircularIndicatorView) findViewById(R.id.indicator);
-
         mController.initialize(slidesNumber);
-        if (selectedIndicatorColor != DEFAULT_COLOR)
-            mController.setSelectedIndicatorColor(selectedIndicatorColor);
-        if (unselectedIndicatorColor != DEFAULT_COLOR)
-            mController.setUnselectedIndicatorColor(unselectedIndicatorColor);
     }
 
-    public void addSlide(@NonNull Fragment fragment) {
+    public void addSlide(Fragment fragment) {
         fragments.add(fragment);
         mPagerAdapter.notifyDataSetChanged();
-    }
-
-    @NonNull
-    public List<Fragment> getSlides() {
-        return mPagerAdapter.getFragments();
-    }
-
-    public boolean isProgressButtonEnabled() {
-        return progressButtonEnabled;
-    }
-
-    public boolean isSkipButtonEnabled() {
-        return skipButtonEnabled;
     }
 
     private void setButtonState(View button, boolean show) {
@@ -214,10 +178,6 @@ public abstract class AppIntro extends AppCompatActivity {
         } else {
             button.setVisibility(View.INVISIBLE);
         }
-    }
-
-    public void setOffScreenPageLimit(int limit) {
-        pager.setOffscreenPageLimit(limit);
     }
 
     public abstract void init(@Nullable Bundle savedInstanceState);
@@ -248,7 +208,7 @@ public abstract class AppIntro extends AppCompatActivity {
      *
      * @param progressButtonEnabled Set true to display. False to hide.
      */
-    public void setProgressButtonEnabled(boolean progressButtonEnabled) {
+    private void setProgressButtonEnabled(boolean progressButtonEnabled) {
         this.progressButtonEnabled = progressButtonEnabled;
         if (progressButtonEnabled) {
             if (pager.getCurrentItem() == slidesNumber - 1) {
@@ -264,213 +224,15 @@ public abstract class AppIntro extends AppCompatActivity {
         }
     }
 
-    /**
-     * Override viewpager bar color
-     *
-     * @param color your color resource
-     */
-    public void setBarColor(@ColorInt final int color) {
-        LinearLayout bottomBar = (LinearLayout) findViewById(R.id.bottom);
-        bottomBar.setBackgroundColor(color);
-    }
-
-    /**
-     * Override skip text
-     *
-     * @param text your text
-     */
-    public void setSkipText(@Nullable final String text) {
-        TextView skipText = (TextView) findViewById(R.id.skip);
-        skipText.setText(text);
-    }
-
-    /**
-     * Override done text
-     *
-     * @param text your text
-     */
-    public void setDoneText(@Nullable final String text) {
+    void setDoneText(@Nullable final String text) {
         TextView doneText = (TextView) findViewById(R.id.done);
         doneText.setText(text);
     }
 
-    /**
-     * Override done button text color
-     *
-     * @param colorDoneText your color resource
-     */
-    public void setColorDoneText(@ColorInt final int colorDoneText) {
-        TextView doneText = (TextView) findViewById(R.id.done);
-        doneText.setTextColor(colorDoneText);
-    }
-
-    /**
-     * Override skip button color
-     *
-     * @param colorSkipButton your color resource
-     */
-    public void setColorSkipButton(@ColorInt final int colorSkipButton) {
-        TextView skip = (TextView) findViewById(R.id.skip);
-        skip.setTextColor(colorSkipButton);
-    }
-
-    /**
-     * Override Next button
-     *
-     * @param imageNextButton your drawable resource
-     */
-    public void setImageNextButton(@DrawableRes final Drawable imageNextButton) {
-        final ImageView nextButton = (ImageView) findViewById(R.id.next);
-        nextButton.setImageDrawable(imageNextButton);
-
-    }
-
-    /**
-     * Allows the user to set the nav bar color of their app intro
-     *
-     * @param Color string form of color in 3 or 6 digit hex form (#ffffff)
-     */
-    public void setNavBarColor(String Color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setNavigationBarColor(android.graphics.Color.parseColor(Color));
-        }
-    }
-
-    /**
-     * Allows the user to set the nav bar color of their app intro
-     *
-     * @param color int form of color. pass your color resource to here (R.color.your_color)
-     */
-    public void setNavBarColor(@ColorRes int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setNavigationBarColor(ContextCompat.getColor(this, color));
-        }
-    }
-
-    /**
-     * Allows for setting statusbar visibility (true by default)
-     *
-     * @param isVisible put true to show status bar, and false to hide it
-     */
-    public void showStatusBar(boolean isVisible) {
-        if (!isVisible) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        } else {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
-    }
-
-    /**
-     * Setting to to display or hide the Skip button. This is a static setting and
-     * button state is maintained across slides until explicitly changed.
-     *
-     * @param showButton Set true to display. False to hide.
-     */
-    public void showSkipButton(boolean showButton) {
-        this.skipButtonEnabled = showButton;
-        setButtonState(skipButton, showButton);
-    }
-
-    /**
-     * Shows or hides Done button, replaced with setProgressButtonEnabled
-     *
-     * @deprecated use {@link #setProgressButtonEnabled(boolean)} instead.
-     */
-    @Deprecated
-    public void showDoneButton(boolean showDone) {
-        setProgressButtonEnabled(showDone);
-    }
-
-    /**
-     * sets vibration when buttons are pressed
-     *
-     * @param vibrationEnabled on/off
-     */
-    public void setVibrate(boolean vibrationEnabled) {
-        this.isVibrateOn = vibrationEnabled;
-    }
-
-    /**
-     * sets vibration intensity
-     *
-     * @param intensity desired intensity
-     */
-    public void setVibrateIntensity(int intensity) {
-        this.vibrateIntensity = intensity;
-    }
-
-    /**
-     * Set a custom {@link CircularIndicatorView} to use a custom indicator view for the {@link AppIntro} instead of the
-     * default one.
-     *
-     * @param controller The controller to use
-     */
-    public void setCustomIndicator(@NonNull CircularIndicatorView controller) {
-        mController = controller;
-    }
-
-    /**
-     * Overrides color of selected and unselected indicator colors
-     * <p/>
-     * Set DEFAULT_COLOR for color value if you don't want to change it
-     *
-     * @param selectedIndicatorColor   your selected color
-     * @param unselectedIndicatorColor your unselected color
-     */
-    public void setIndicatorColor(int selectedIndicatorColor, int unselectedIndicatorColor) {
-        this.selectedIndicatorColor = selectedIndicatorColor;
-        this.unselectedIndicatorColor = unselectedIndicatorColor;
-
-        if (mController != null) {
-            if (selectedIndicatorColor != DEFAULT_COLOR)
-                mController.setSelectedIndicatorColor(selectedIndicatorColor);
-            if (unselectedIndicatorColor != DEFAULT_COLOR)
-                mController.setUnselectedIndicatorColor(unselectedIndicatorColor);
-        }
-    }
-
-    /**
-     * Setting to disable forward swiping right on current page and allow swiping left. If a swipe
-     * left occurs, the lock state is reset and swiping is re-enabled. (one shot disable) This also
-     * hides/shows the Next and Done buttons accordingly.
-     *
-     * @param lockEnable Set true to disable forward swiping. False to enable.
-     */
-    public void setNextPageSwipeLock(boolean lockEnable) {
-        if (lockEnable) {
-            // if locking, save current progress button visibility
-            baseProgressButtonEnabled = progressButtonEnabled;
-            setProgressButtonEnabled(!lockEnable);
-        } else {
-            // if unlocking, restore original button visibility
-            setProgressButtonEnabled(baseProgressButtonEnabled);
-        }
-        pager.setNextPagingEnabled(!lockEnable);
-    }
-
-    /**
-     * Setting to disable swiping left and right on current page. This also
-     * hides/shows the Next and Done buttons accordingly.
-     *
-     * @param lockEnable Set true to disable forward swiping. False to enable.
-     */
-    public void setSwipeLock(boolean lockEnable) {
-        if (lockEnable) {
-            // if locking, save current progress button visibility
-            baseProgressButtonEnabled = progressButtonEnabled;
-            //setProgressButtonEnabled(!lockEnable);
-        } else {
-            // if unlocking, restore original button visibility
-            setProgressButtonEnabled(baseProgressButtonEnabled);
-        }
-        pager.setPagingEnabled(!lockEnable);
-    }
-
-    static class PagerAdapter extends FragmentPagerAdapter {
+    private static class PagerAdapter extends FragmentPagerAdapter {
         List<Fragment> fragments;
 
-        public PagerAdapter(FragmentManager fm, @NonNull List<Fragment> fragments) {
+        PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
             super(fm);
             this.fragments = fragments;
         }
@@ -485,9 +247,5 @@ public abstract class AppIntro extends AppCompatActivity {
             return this.fragments.size();
         }
 
-        @NonNull
-        public List<Fragment> getFragments() {
-            return fragments;
-        }
     }
 }
