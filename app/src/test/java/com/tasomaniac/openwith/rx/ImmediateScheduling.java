9c07ca8d53a6ae0e0e131fea7edd1e9ae717369d
Said Tahsin Dane <tasomaniac@gmail.com>
2017-05-13 20:02:14 +0200

    move ImmediateScheduling to test package

--- app/src/test/java/com/tasomaniac/openwith/rx/ImmediateScheduling.java
+++ app/src/test/java/com/tasomaniac/openwith/rx/ImmediateScheduling.java
@@ -2,7 +2,7 @@ package com.tasomaniac.openwith.rx;
 
 import static io.reactivex.schedulers.Schedulers.trampoline;
 
-public class ImmediateScheduling extends SchedulingStrategy {
+public final class ImmediateScheduling extends SchedulingStrategy {
 
     public ImmediateScheduling() {
         super(trampoline(), trampoline());
