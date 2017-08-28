83328f7c113e6ea2eb3b57b912a7821b21cee6ec
Said Tahsin Dane <tasomaniac@gmail.com>
2017-08-28 22:59:38 +0200

    Extract the ViewHolder and convert it to Kotlin

--- app/src/main/java/com/tasomaniac/openwith/resolver/ResolveListAdapter.java
+++ app/src/main/java/com/tasomaniac/openwith/resolver/ResolveListAdapter.java
@@ -3,19 +3,12 @@ package com.tasomaniac.openwith.resolver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import com.tasomaniac.openwith.R;
 
 import java.util.Collections;
 import java.util.List;
 
-public class ResolveListAdapter extends RecyclerView.Adapter<ResolveListAdapter.ViewHolder> {
+public class ResolveListAdapter extends RecyclerView.Adapter<ApplicationViewHolder> {
 
     private List<DisplayActivityInfo> mList = Collections.emptyList();
     private boolean displayExtendedInfo = false;
@@ -40,12 +33,12 @@ public class ResolveListAdapter extends RecyclerView.Adapter<ResolveListAdapter.
     }
 
     @NonNull @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return ViewHolder.create(parent);
+    public ApplicationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return ApplicationViewHolder.Companion.create(parent, iconLoader, displayExtendedInfo);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull List<Object> payloads) {
+    public void onBindViewHolder(ApplicationViewHolder holder, int position, List<Object> payloads) {
         super.onBindViewHolder(holder, position, payloads);
 
         boolean checked = position == checkedItemPosition;
@@ -53,30 +46,16 @@ public class ResolveListAdapter extends RecyclerView.Adapter<ResolveListAdapter.
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
-        final DisplayActivityInfo info = mList.get(position);
+    public void onViewRecycled(ApplicationViewHolder holder) {
+        holder.unbind();
+    }
 
-        holder.text.setText(info.displayLabel());
-        if (holder.text2 != null) {
-            if (displayExtendedInfo) {
-                holder.text2.setVisibility(View.VISIBLE);
-                holder.text2.setText(info.extendedInfo());
-            } else {
-                holder.text2.setVisibility(View.GONE);
-            }
-        }
-        if (holder.icon != null) {
-            holder.icon.setImageDrawable(info.displayIcon());
-        }
-
-        holder.itemView.setOnClickListener(v -> {
-            int adapterPosition = holder.getAdapterPosition();
-            itemClickListener.onItemClick(info);
-            setItemChecked(adapterPosition);
-        });
-
-        holder.itemView.setOnLongClickListener(v -> itemLongClickListener != null
-                && itemLongClickListener.onItemLongClick(info));
+    @Override
+    public void onBindViewHolder(ApplicationViewHolder holder, int position) {
+        holder.bind(mList.get(position), dri -> {
+            itemClickListener.onItemClick(dri);
+            setItemChecked(holder.getAdapterPosition());
+        }, itemLongClickListener);
     }
 
     public void setItemClickListener(@Nullable ItemClickListener itemClickListener) {
@@ -116,26 +95,4 @@ public class ResolveListAdapter extends RecyclerView.Adapter<ResolveListAdapter.
         mList.remove(item);
         notifyItemRemoved(position);
     }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.text1)
-        TextView text;
-        @Nullable
-        @BindView(R.id.text2)
-        TextView text2;
-        @Nullable
-        @BindView(R.id.icon)
-        ImageView icon;
-
-        public static ViewHolder create(ViewGroup parent) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.resolve_list_item, parent, false);
-            return new ViewHolder(view);
-        }
-
-        private ViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-        }
-    }
-
 }
