d52a47f8701b251345309b9843c69fdf9a0426fa
Said Tahsin Dane <tasomaniac@gmail.com>
2017-11-21 09:33:26 +0100

    Support Amazon URLs with product keyword in the middle

--- app/src/main/java/com/tasomaniac/openwith/util/Urls.java
+++ app/src/main/java/com/tasomaniac/openwith/util/Urls.java
@@ -144,16 +144,10 @@ public final class Urls {
         @Nullable
         static String extractAmazonASIN(String foundUrl) {
             try {
-                final Matcher matcher = Pattern.compile(".*//www.amazon.(?:com|co\\.uk|co.jp|com\\.au|com\\.br|ca|cn|fr|de|in|it|com\\.mx|nl|es)/gp/aw/d/(\\w{10})/.*", Pattern.CASE_INSENSITIVE)
-                        .matcher(foundUrl);
-                if (matcher.find()) {
-                    return matcher.group(1);
-                }
-            } catch (Exception ignored) {
-            }
-            try {
-                //http://www.amazon.com/Garmin-Speed-Cadence-Bike-Sensor/dp/B000BFNOT8
-                final Matcher matcher = Pattern.compile(".*//www.amazon.(?:com|co\\.uk|co.jp|com\\.au|com\\.br|ca|cn|fr|de|in|it|com\\.mx|nl|es)/(?:.+/)?dp/(\\w{10}).*", Pattern.CASE_INSENSITIVE)
+                // https://www.amazon.de/gp/product/B01LYYM9I3
+                // https://www.amazon.com/gp/aw/d/B001GNBJQO?vs=1
+                // http://www.amazon.com/Garmin-Speed-Cadence-Bike-Sensor/dp/B000BFNOT8
+                final Matcher matcher = Pattern.compile(".*//www.amazon.(?:com|co\\.uk|co.jp|com\\.au|com\\.br|ca|cn|fr|de|in|it|com\\.mx|nl|es)/(?:.+/)?(?:gp/aw/d|gp/product|dp)+/(\\w{10}).*", Pattern.CASE_INSENSITIVE)
                         .matcher(foundUrl);
                 if (matcher.find()) {
                     return matcher.group(1);
