c92cb1e4cdb5b60a197047b88055fb9b6f0d46b8
Said Tahsin Dane <tasomaniac@gmail.com>
2017-12-02 00:13:32 +0100

    Remove automatic night mode option

--- app/src/main/java/com/tasomaniac/openwith/settings/NightModePreferences.java
+++ app/src/main/java/com/tasomaniac/openwith/settings/NightModePreferences.java
@@ -2,6 +2,7 @@ package com.tasomaniac.openwith.settings;
 
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatDelegate;
 
@@ -14,14 +15,12 @@ public class NightModePreferences {
     private final SharedPreferences sharedPreferences;
     private final Resources resources;
     private final String key;
-    private final String defaultValue;
 
     @Inject
     public NightModePreferences(SharedPreferences sharedPreferences, Resources resources) {
         this.sharedPreferences = sharedPreferences;
         this.resources = resources;
         this.key = resources.getString(R.string.pref_key_night_mode);
-        this.defaultValue = Mode.OFF.stringVale(resources);
     }
 
     public void updateDefaultNightMode() {
@@ -34,14 +33,14 @@ public class NightModePreferences {
     }
 
     public Mode getMode() {
-        String value = sharedPreferences.getString(key, defaultValue);
-        return Mode.fromValue(resources, value);
+        String value = sharedPreferences.getString(key, null);
+        Mode mode = Mode.fromValue(resources, value);
+        return mode == null ? Mode.OFF : mode;
     }
 
     public enum Mode {
         OFF(R.string.pref_value_night_mode_off, R.string.pref_entry_night_mode_off, AppCompatDelegate.MODE_NIGHT_NO),
-        ON(R.string.pref_value_night_mode_on, R.string.pref_entry_night_mode_on, AppCompatDelegate.MODE_NIGHT_YES),
-        AUTO(R.string.pref_value_night_mode_auto, R.string.pref_entry_night_mode_auto, AppCompatDelegate.MODE_NIGHT_AUTO);
+        ON(R.string.pref_value_night_mode_on, R.string.pref_entry_night_mode_on, AppCompatDelegate.MODE_NIGHT_YES);
 
         @StringRes private final int value;
         private final int entry;
@@ -57,6 +56,7 @@ public class NightModePreferences {
             return resources.getString(value);
         }
 
+        @Nullable
         static Mode fromValue(Resources resources, String value) {
             for (Mode mode : Mode.values()) {
                 if (mode.stringVale(resources).equals(value)) {
