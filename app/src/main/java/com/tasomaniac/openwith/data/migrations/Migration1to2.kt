f0ee8493156835d7a3b9fad23de754481726e4ae
Said Tahsin Dane <tasomaniac@gmail.com>
2018-03-27 18:42:55 +0200

    keep a default value for preferred

--- app/src/main/java/com/tasomaniac/openwith/data/migrations/Migration1to2.kt
+++ app/src/main/java/com/tasomaniac/openwith/data/migrations/Migration1to2.kt
@@ -6,13 +6,13 @@ import android.arch.persistence.room.migration.Migration
 object Migration1to2 : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL(
-            "CREATE TABLE `openwith_temp` (`_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `host` TEXT NOT NULL, `component` TEXT NOT NULL, `preferred` INTEGER NOT NULL, `last_chosen` INTEGER NOT NULL)"
+            "CREATE TABLE `openwith_backup` (`_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `host` TEXT NOT NULL, `component` TEXT NOT NULL, `preferred` INTEGER NOT NULL DEFAULT 0, `last_chosen` INTEGER NOT NULL)"
         )
         database.execSQL(
-            "INSERT INTO openwith_temp (_id, host, component, preferred, last_chosen) SELECT * FROM openwith"
+            "INSERT INTO openwith_backup (_id, host, component, preferred, last_chosen) SELECT * FROM openwith"
         )
         database.execSQL("DROP TABLE openwith")
-        database.execSQL("ALTER TABLE openwith_temp RENAME TO openwith")
+        database.execSQL("ALTER TABLE openwith_backup RENAME TO openwith")
         database.execSQL("CREATE UNIQUE INDEX `index_openwith_host` ON `openwith` (`host`)")
     }
 
