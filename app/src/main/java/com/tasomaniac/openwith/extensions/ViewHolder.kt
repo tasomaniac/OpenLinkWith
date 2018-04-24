7ae8a8de0913fa731b79d57bf229ef331706f2d9
Said Tahsin Dane <tasomaniac@gmail.com>
2018-04-24 16:06:20 -0400

    suppress magic number

--- app/src/main/java/com/tasomaniac/openwith/extensions/ViewHolder.kt
+++ app/src/main/java/com/tasomaniac/openwith/extensions/ViewHolder.kt
@@ -3,6 +3,7 @@ package com.tasomaniac.openwith.extensions
 import android.support.v7.widget.RecyclerView
 import com.tasomaniac.openwith.util.Utils
 
+@Suppress("MagicNumber")
 fun <T : RecyclerView.ViewHolder> T.withMinHeight() = apply {
     itemView.minimumHeight = Utils.dpToPx(itemView.resources, 72)
 }
