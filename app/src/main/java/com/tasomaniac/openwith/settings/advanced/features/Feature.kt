4feb1dd7b7c52fede2c664a1409c2f74914506e5
Said Tahsin Dane <tasomaniac@gmail.com>
2018-08-18 21:44:10 +0300

    Add images for each Feature

--- app/src/main/java/com/tasomaniac/openwith/settings/advanced/features/Feature.kt
+++ app/src/main/java/com/tasomaniac/openwith/settings/advanced/features/Feature.kt
@@ -1,27 +1,32 @@
 package com.tasomaniac.openwith.settings.advanced.features
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.tasomaniac.openwith.R
 
 enum class Feature(
     @StringRes val titleRes: Int,
     @StringRes val detailsRes: Int,
+    @DrawableRes val imageRes: Int,
     val prefKey: String
 ) {
 
     ADD_TO_HOMESCREEN(
         R.string.pref_title_feature_add_to_homescreen,
         R.string.pref_details_feature_add_to_homescreen,
+        R.drawable.tutorial_4,
         "pref_feature_add_to_homescreen"
     ),
     TEXT_SELECTION(
         R.string.pref_title_feature_text_selection,
         R.string.pref_details_feature_text_selection,
+        R.drawable.tutorial_4, // TODO add text selection image
         "pref_feature_text_selection"
     ),
     DIRECT_SHARE(
         R.string.pref_title_feature_direct_share,
         R.string.pref_details_feature_direct_share,
+        R.drawable.tutorial_4, // TODO add direct share image
         "pref_feature_direct_share"
     )
 
--- app/src/main/java/com/tasomaniac/openwith/settings/advanced/features/ToggleFeatureActivity.kt
+++ app/src/main/java/com/tasomaniac/openwith/settings/advanced/features/ToggleFeatureActivity.kt
@@ -9,6 +9,7 @@ import androidx.preference.Preference
 import com.tasomaniac.openwith.R
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.toggle_feature_activity.featureDetails
+import kotlinx.android.synthetic.main.toggle_feature_activity.featureImage
 import kotlinx.android.synthetic.main.toggle_feature_activity.featureToggle
 import kotlinx.android.synthetic.main.toggle_feature_activity.featureToggleText
 import kotlinx.android.synthetic.main.toggle_feature_activity.toolbar
@@ -39,6 +40,7 @@ class ToggleFeatureActivity : DaggerAppCompatActivity() {
         setTitle(feature.titleRes)
 
         featureDetails.text = getString(feature.detailsRes).parseAsHtml()
+        featureImage.setImageResource(feature.imageRes)
     }
 
     @StringRes
