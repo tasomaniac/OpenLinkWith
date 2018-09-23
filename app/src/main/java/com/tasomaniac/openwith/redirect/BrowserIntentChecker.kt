7d2569bcf1ffe50c2bab13745fe4b34b76740939
Said Tahsin Dane <tasomaniac@gmail.com>
2018-09-23 19:07:54 +0200

    Fix problem with browser checker.

--- app/src/main/java/com/tasomaniac/openwith/redirect/BrowserIntentChecker.kt
+++ app/src/main/java/com/tasomaniac/openwith/redirect/BrowserIntentChecker.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.M
+import com.tasomaniac.openwith.BuildConfig
 import com.tasomaniac.openwith.browser.resolver.BrowserResolver
 import com.tasomaniac.openwith.util.Intents
 import com.tasomaniac.openwith.util.componentName
@@ -19,11 +20,22 @@ class BrowserIntentChecker @Inject constructor(
         if (!Intents.isHttp(sourceIntent)) {
             return false
         }
-        val flag = if (SDK_INT >= M) PackageManager.MATCH_ALL else PackageManager.MATCH_DEFAULT_ONLY
-        val resolved = packageManager.queryIntentActivities(sourceIntent, flag).toComponents()
+        val resolved = resolveSource(sourceIntent).toComponents()
         val browsers = browserResolver.queryBrowsers().toComponents()
 
         return (resolved - browsers).isEmpty()
+    }
+
+    private fun resolveSource(sourceIntent: Intent): List<ResolveInfo> {
+        val flag = if (SDK_INT >= M) PackageManager.MATCH_ALL else PackageManager.MATCH_DEFAULT_ONLY
+        val resolved = packageManager.queryIntentActivities(sourceIntent, flag)
+
+        resolved.removeAll {
+            it.activityInfo.packageName == BuildConfig.APPLICATION_ID
+        }
+        return resolved.distinctBy {
+            it.activityInfo.packageName
+        }
     }
 
     private fun List<ResolveInfo>.toComponents() = map { it.activityInfo.componentName() }
