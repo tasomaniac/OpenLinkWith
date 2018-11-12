fd67178b57754bdb6333fbf30a6f31ddc0066b6e
Said Tahsin Dane <tasomaniac@gmail.com>
2018-11-13 00:37:25 +0100

    Add ask for rating as an optional item in the list.
    UI is ready already. Functionaly is missing.
    
    Should persist state
    Should have heuristics about when to display this

---
+++ app/src/main/java/com/tasomaniac/openwith/settings/rating/AskForRatingSettings.kt
@@ -0,0 +1,100 @@
+package com.tasomaniac.openwith.settings.rating
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ShareCompat
+import androidx.preference.PreferenceCategory
+import com.tasomaniac.openwith.R
+import com.tasomaniac.openwith.data.Analytics
+import com.tasomaniac.openwith.settings.Settings
+import com.tasomaniac.openwith.settings.SettingsFragment
+import javax.inject.Inject
+
+class AskForRatingSettings @Inject constructor(
+    fragment: SettingsFragment,
+    private val analytics: Analytics
+) : Settings(fragment) {
+
+    private var preferenceCategory: PreferenceCategory? = null
+
+    override fun setup() {
+        updateButton()
+    }
+
+    override fun release() {
+    }
+
+    private fun updateButton() {
+        val shouldDisplay = shouldDisplay()
+
+        if (shouldDisplay.not() && isAdded()) {
+            remove()
+        }
+
+        if (shouldDisplay) {
+            if (!isAdded()) {
+                addAskForRatingPreference()
+                analytics.sendEvent("AskForRating", "Added", "New")
+            }
+
+        }
+    }
+
+    private fun shouldDisplay() = true
+
+    private fun addAskForRatingPreference() {
+        addPreferencesFromResource(R.xml.pref_ask_for_rating)
+        preferenceCategory = findPreference(R.string.pref_key_category_ask_for_rating) as PreferenceCategory
+        setupRatingBar()
+    }
+
+    private fun setupRatingBar() {
+        val preference = findPreference(R.string.pref_key_ask_for_rating) as AskForRatingPreference
+        preference.onRatingChange = ::handleRatingChange
+    }
+
+    private fun handleRatingChange(rating: Float) {
+        if (rating >= GOOD_RATING) {
+            context.startActivity(STORE_INTENT)
+            remove()
+        } else {
+            AlertDialog.Builder(context)
+                .setTitle(R.string.ask_for_rating_feedback)
+                .setMessage(R.string.ask_for_rating_feedback_message)
+                .setNegativeButton(R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    startContactEmailChooser()
+                }
+                .setOnDismissListener {
+                    remove()
+                }
+                .show()
+        }
+
+        analytics.sendEvent("AskForRating", "Rating Clicked", rating.toString())
+    }
+
+    private fun startContactEmailChooser() {
+        ShareCompat.IntentBuilder.from(activity)
+            .addEmailTo("Said Tahsin Dane <tasomaniac+openlinkwith@gmail.com>")
+            .setSubject(context.getString(R.string.ask_for_rating_feedback_email_subject))
+            .setType("message/rfc822")
+            .startChooser()
+    }
+
+    private fun remove() {
+        removePreference(preferenceCategory!!)
+        preferenceCategory = null
+    }
+
+    private fun isAdded() = preferenceCategory != null
+
+    companion object {
+        private val STORE_INTENT = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://play.google.com/store/apps/details?id=com.tasomaniac.openwith")
+        )
+        private const val GOOD_RATING = 4
+    }
+}
