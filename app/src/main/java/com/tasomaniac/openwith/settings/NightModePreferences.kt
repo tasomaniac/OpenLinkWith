19a6733d9a69fa56c26465bef574605866b416be
Said Tahsin Dane <tasomaniac@gmail.com>
2019-03-17 23:30:27 +0100

    Introduce new Night Mode options

--- app/src/main/java/com/tasomaniac/openwith/settings/NightModePreferences.kt
+++ app/src/main/java/com/tasomaniac/openwith/settings/NightModePreferences.kt
@@ -2,10 +2,11 @@ package com.tasomaniac.openwith.settings
 
 import android.content.SharedPreferences
 import android.content.res.Resources
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.P
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatDelegate
 import com.tasomaniac.openwith.R
-
 import javax.inject.Inject
 
 class NightModePreferences @Inject constructor(
@@ -22,8 +23,13 @@ class NightModePreferences @Inject constructor(
     val mode: Mode
         get() {
             val value = sharedPreferences.getString(key, null)
-            return Mode.fromValue(resources, value) ?: Mode.OFF
+            return Mode.fromValue(resources, value) ?: defaultNightMode()
         }
+
+    private fun defaultNightMode() = when {
+        SDK_INT >= P -> Mode.SYSTEM
+        else -> Mode.OFF
+    }
 
     fun updateDefaultNightMode() {
         AppCompatDelegate.setDefaultNightMode(mode.delegate)
@@ -35,7 +41,17 @@ class NightModePreferences @Inject constructor(
         val delegate: Int
     ) {
         OFF(R.string.pref_value_night_mode_off, R.string.pref_entry_night_mode_off, AppCompatDelegate.MODE_NIGHT_NO),
-        ON(R.string.pref_value_night_mode_on, R.string.pref_entry_night_mode_on, AppCompatDelegate.MODE_NIGHT_YES);
+        ON(R.string.pref_value_night_mode_on, R.string.pref_entry_night_mode_on, AppCompatDelegate.MODE_NIGHT_YES),
+        BATTERY(
+            R.string.pref_value_night_mode_battery,
+            R.string.pref_entry_night_mode_battery,
+            AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+        ),
+        SYSTEM(
+            R.string.pref_value_night_mode_system,
+            R.string.pref_entry_night_mode_system,
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        );
 
         fun stringVale(resources: Resources): String = resources.getString(value)
 
