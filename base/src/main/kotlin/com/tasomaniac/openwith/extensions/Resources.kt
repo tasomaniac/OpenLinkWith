480cf4f1f77c2155a45b4196cb4224fc39003328
Said Tahsin Dane <tasomaniac@gmail.com>
2019-10-26 01:10:26 +0200

    fix lint issues and run static analysis for all modules

--- base/src/main/kotlin/com/tasomaniac/openwith/extensions/Resources.kt
+++ base/src/main/kotlin/com/tasomaniac/openwith/extensions/Resources.kt
@@ -3,7 +3,6 @@ package com.tasomaniac.openwith.extensions
 import android.content.res.Resources
 import android.util.TypedValue
 
-
 fun Resources.dpToPx(dp: Int): Int {
     return TypedValue
         .applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), displayMetrics).toInt()
